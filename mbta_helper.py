53e51630ce8bacf71db67d1bce41d597b9f68d19
Rumeer <35002782+Rumeer@users.noreply.github.com>
2020-04-05 20:29:34 -0400

    MBTA_Part1

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,55 +1,64 @@
-# Useful URLs (you need to add the appropriate parameters for your requests)
-MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
-MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
-
-# Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = "JNVOQp6Yig9sib6RVBnpbBpbirMOzyG1"
-MBTA_API_KEY = ""
+import urllib.request
+import urllib.parse
+import json
+from pprint import pprint
 
 
-# A little bit of scaffolding if you want to use it
-
-def get_json(url):
-    """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
-    """
-    pass
+def takeUserInput(key):
+    secondParam = input("Enter the location:")
+    params = {'key': key, 'location': secondParam}
+    a = urllib.parse.urlencode(params)
+    return (f'http://www.mapquestapi.com/geocoding/v1/address?' + str(a))
 
 
-def get_lat_long(place_name):
-    """
-    Given a place name or address, return a (latitude, longitude) tuple
-    with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding  API URL formatting requirements.
-    """
-    pass
+def getCoordinates():
 
+    MAPQUEST_API_KEY = 'JNVOQp6Yig9sib6RVBnpbBpbirMOzyG1'
+    url = takeUserInput(MAPQUEST_API_KEY)
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    pprint(response_data)
 
-def get_nearest_station(latitude, longitude):
-    """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
-    tuple for the nearest MBTA station to the given coordinates.
-    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
-    formatting requirements for the 'GET /stops' API.
-    """
-    pass
+    locLen = 0  # sections counts the number of locations
+    for x in (response_data["results"][0]["locations"]):
+        locLen = locLen + 1
+    print ("LocLen is " + str(locLen))
+    count = 0
+    if locLen ==0: print("No location found.")
+    elif locLen ==1:  #condition when one location comes up thats not MA
+        if response_data["results"][0]["locations"][0]["adminArea3"] != 'MA':
+            print("ERROR: MA LOCATION NOT FOUND")
+            exit() # exits code because you can't do anything
+    else: #when there is more than one location
+        while(count != locLen-1):
+            if response_data["results"][0]["locations"][count]["adminArea3"] == 'MA':
+                break
+            else:
+                count = count + 1
 
+        if response_data["results"][0]["locations"][count]["adminArea3"] != 'MA': # exists if last loc is not MA
+            print("ERROR: MA LOCATION NOT FOUND.")
+            exit()
+    return response_data["results"][0]["locations"][count]["displayLatLng"]
 
-def find_stop_near(place_name):
-    """
-    Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
-    """
-    pass
+def getNearestLocation(latLngArray):
+    mbta_api_key = 'd657ba4068af473bb42cff137b163785'
+    url = f'https://api-v3.mbta.com/stops?api_key=9d7fbfd56c164a2bbedb3691a1a79949&filter%5Blatitude%5D='\
+          +str(latLngArray['lat'])+'&filter%5Blongitude%5D='\
+          +str(latLngArray['lng'])
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    pprint(response_data)
+    print(response_data["data"][0]["attributes"]["name"])
+    if response_data["data"][0]["attributes"]["wheelchair_boarding"] == 0:
+        print("No information on wheelchair boarding")
+    elif response_data["data"][0]["attributes"]["wheelchair_boarding"] == 1:
+        print("	Accessible (if trip is wheelchair accessible)")
+    else:
+        print("Inacessible")
 
-
-def main():
-    """
-    You can test all the functions here
-    """
-    pass
-
-
-if __name__ == '__main__':
-    main()
+latLngArray = (getCoordinates())
+print("-------------------------------------------------------------------------------------------------------------")
+getNearestLocation(latLngArray)
