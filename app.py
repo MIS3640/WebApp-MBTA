d8aa43de56273b4b07b8b777706382f5098fcf6e
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-04-09 20:47:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-"""
-Simple "Hello, World" application using Flask
-"""
-
 from flask import Flask
+from flask import request
+from flask import render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+# @app.route('/get_nearest_station/', methods=['GET', 'POST'])
