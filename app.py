dfd83632d34ee5e84ab22fdaa1f12c376d5abcdb
catherineexu <catherineexu@users.noreply.github.com>
2020-04-12 15:23:45 -0700

    Complete

--- app.py
+++ app.py
@@ -2,12 +2,50 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for
+from wtforms import Form, StringField, validators
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
+class MbtaForm(Form):
+    """
+    Represents a form that requires a user's input of a location or address.
+    """
+    placename = StringField('Please enter a location or address:', [validators.DataRequired()])
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home_page():
+    """
+    Returns a template that renders the home page with the MBTA form. 
+    """
+    form = MbtaForm(request.form)
+    return render_template('index.html', form=form)
+    
+@app.route('/nearest', methods=['POST'])
+def find_nearest_stops():
+    """
+    Takes in the form with the user's input. 
+    If the form is valid, redirect the request to the handler for finding the nearest MBTA stop.
+    Otherwise, render the home page with the form with errors. 
+    """
+    form = MbtaForm(request.form)
+    if form.validate():
+        return redirect(url_for('find_nearest_mbta_stop'), code=307)
+    else:
+        return render_template('index.html', form=form)
+
+@app.route('/nearest_mbta', methods=['POST'])
+def find_nearest_mbta_stop():
+    """
+    Takes in a form with a field for a place name, and tries to find the closest MBTA stop. 
+    If such MBTA stop exists, return template that renders a page with the stop name and if it's wheelchair accessible.
+    Otherwise, return template that renders an error page with a link to the home page.
+    """
+    place_name = request.form.get('placename')
+    try:
+        stop_name, is_wheelchair_accessible = find_stop_near(place_name)
+        print(stop_name, is_wheelchair_accessible)
+        return render_template('nearest_mbta.html', stop_name=stop_name, wheelchair_accessible=is_wheelchair_accessible)
+    except:
+        return render_template('error.html')
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,10 +1,14 @@
+import urllib.request
+import json
+from pprint import pprint
+
 # Useful URLs (you need to add the appropriate parameters for your requests)
-MAPQUEST_BASE_URL = "http://open.mapquestapi.com/geocoding/v1/address"
+MAPQUEST_BASE_URL = "https://open.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = "JLP0dYNyUhZXHbTzv2vzs4s3XHJbBlk9"
+MBTA_API_KEY = "c1af34d3361b4e5a8140a22fbe55b989"
 
 
 # A little bit of scaffolding if you want to use it
@@ -14,8 +18,12 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
-
+    print("Getting {}".format(url))
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    # pprint(response_data)
+    return response_data
 
 def get_lat_long(place_name):
     """
@@ -24,32 +32,59 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+    params = {
+        'key': MAPQUEST_API_KEY, 
+        'location': place_name 
+    }
+    encoded_params = urllib.parse.urlencode(params)
+    url = MAPQUEST_BASE_URL +  '?'  + encoded_params
+    data = get_json(url)
+    lat_long = data.get('results')[0].get('locations')[0].get('latLng')
+    return (lat_long.get('lat'), lat_long.get('lng'))
 
-
-def get_nearest_station(latitude, longitude):
+def get_nearest_stop(latitude, longitude):
     """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
-    tuple for the nearest MBTA station to the given coordinates.
+    Given latitude and longitude strings, return a (stop_name, wheelchair_accessible)
+    tuple for the nearest MBTA stop to the given coordinates.
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    params = {
+        'api_key': MBTA_API_KEY,
+        'filter[latitude]': latitude, 
+        'filter[longitude]': longitude,
+        'filter[radius]': 20,
+        'sort': 'distance',
+        'page[limit]': 1
+    }
+    encoded_params = urllib.parse.urlencode(params)
+    url = MBTA_BASE_URL + '?' + encoded_params
+    data = get_json(url)
 
+    closest_stop = data.get('data')[0]
+    attributes = closest_stop.get('attributes')
+    return (attributes.get('name'), attributes.get('wheelchair_boarding') == 1)
 
 def find_stop_near(place_name):
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
     """
-    pass
-
+    lat, long = get_lat_long(place_name)
+    stop_name, wheelchair_accessible = get_nearest_stop(str(lat), str(long))
+    print("Closest stop is: {}".format(stop_name))
+    if wheelchair_accessible:
+        print("stop is wheelchair accessible")
+    else:
+        print("Stop is not wheelchair accessible")
+    
+    return (stop_name, wheelchair_accessible)
 
 def main():
     """
     You can test all the functions here
     """
-    pass
-
+    place_name = input('Enter a place or address:\n')
+    find_stop_near(place_name)
 
 if __name__ == '__main__':
     main()
