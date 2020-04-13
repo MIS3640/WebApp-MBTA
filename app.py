5ef49e2aa104974f15b19c394260ad63ca53036e
takakikato <37848247+takakikato@users.noreply.github.com>
2020-04-13 07:39:34 +0630

    projject complete

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-"""
-Simple "Hello, World" application using Flask
-"""
-
-from flask import Flask
-
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near, get_schedule, get_distance
 
 app = Flask(__name__)
 
+@app.route('/', methods=["GET", "POST"])
+def indext():
+    if request.method == "POST":
+        location = str(request.form["location"])
+        stop = find_stop_near(location)[0]
+        acc = find_stop_near(location)[1]
+        if acc == 1:
+            acc = "Yes"
+        elif acc == 2:
+            acc = "No"
+        else:
+            acc = "Unknown"
+        schedule = get_schedule(location)
+        distance = get_distance(location)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+        if stop:
+            return render_template(
+                "nearest.html", 
+                location = location, 
+                stop = stop,
+                acc = acc,
+                schedule = schedule,
+                distance = distance 
+            )
+        else:
+            return render_template(
+                "index.html",
+                error = True
+            )
+    return render_template(
+        "index.html",
+        error = None
+    )
