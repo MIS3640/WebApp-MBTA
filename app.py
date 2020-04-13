f3e5d19910386d2b5e0973248a1bce00ff93e515
Fu <hfu3@babson.edu>
2020-04-12 21:27:09 -0400

    Work

--- app.py
+++ app.py
@@ -2,43 +2,30 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request, session, url_for, redirect 
 
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
+app.secret_key = "SecretKey"
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods = ['POST','GET'])
 
-
-import urllib.request
-import json
-from pprint import pprint
-
-MAPQUEST_API_KEY = 'bvd5kR5ANCpY295vIH5qgDEcpKZzeuKR'
-
-url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
-f = urllib.request.urlopen(url)
-response_text = f.read().decode('utf-8')
-response_data = json.loads(response_text)
-# pprint(response_data)
-
-# 1. Write a function (maybe two) to extract the latitude 
-# and longitude from the JSON response.
-
-# 2. Write a function that takes an address or place name 
-# as input and returns a properly encoded URL to make 
-# a MapQuest geocode request.
-
-# 3. Write a function that takes a latitude and longitude 
-# and returns the name of the closest MBTA stop and 
-# whether it is wheelchair accessible
-
-# 4. Combine your functions from the previous sections 
-# to create a tool that takes a place name or address 
-# as input, finds its latitude/longitude, and returns 
-# the nearest MBTA stop and whether it is wheelchair 
-# accessible.
-
+def index(): 
+    """
+    Home page of site
+    """
+    if request.method =='POST': 
+        session['place'] = request.form['place']
+        return redirect(url_for('nearest_mbta'))
+    else: 
+        return render_template('index.html')
+        
+@app.route('/nearest_mbta')
+def nearest_mbta(): 
+    """
+    returns nearest mbta staion
+    """
+    text = find_stop_near(session['place'])
+    return render_template('place.html', text = text)
