1f62a9d61146f5a3d6dfa04bb6c8c581117c2127
nnakahara <nakaharanathan@gmail.com>
2020-04-12 18:45:59 -0700

    Finish Assignment 3

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-"""
-Simple "Hello, World" application using Flask
-"""
-
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for, session
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
+app.secret_key = "theworldsmostsecretkey" # Secret Key required for session["place"] to store info
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/',methods=['POST','GET'])
+def home():
+    """ 
+        Home page of the website
+    """
+    if request.method =='POST':
+        session["place"] = request.form["place"] # store "place" input
+        return redirect(url_for("nearest"))
+    else:
+        return render_template("index.html")
+
+
+@app.route("/nearest")
+def nearest():
+    """ 
+        Returns any nearby MTBA stations if possible
+    """
+    try:
+        text = find_stop_near(session["place"])
+        return render_template("place.html", text = text)
+    except:
+        return render_template('error.html')
--- mbta_helper.py
+++ mbta_helper.py
@@ -3,9 +3,13 @@ MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = "IdUzr7JMaCsMSeqpxdxcwbR5x3n1zq6H"
+MBTA_API_KEY = "e8af8d0f8edb4284bd0e85524cb9a348"
 
+# Import Packages
+import urllib.request
+import json
+from pprint import pprint
 
 # A little bit of scaffolding if you want to use it
 
@@ -14,7 +18,10 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
 
 
 def get_lat_long(place_name):
@@ -24,7 +31,10 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+    place_name = place_name.replace(" ", "%20") # Remove formatting for address
+    latLng = get_json(f"http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place_name}")["results"][0]["locations"][0]["latLng"]
+    return latLng["lat"], latLng["lng"]
+    
 
 
 def get_nearest_station(latitude, longitude):
@@ -34,14 +44,22 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    response_data = get_json(f"{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance")["data"][0]['attributes']
+    return response_data["name"], response_data["wheelchair_boarding"]
 
 
 def find_stop_near(place_name):
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
     """
-    pass
+    response_data = get_nearest_station(get_lat_long(place_name)[0], get_lat_long(place_name)[1])
+    if response_data[1] == 0:
+        accessible_info = "Unkown if Wheelchair accessible"
+    elif response_data[1] == 1:
+        accessible_info = "Wheelchair accessible"
+    elif response_data[1] == 2:
+        accessible_info = "Wheelchair inaccessible"
+    return f"The nearest station to {place_name} is {response_data[0]}, and is {accessible_info}"
 
 
 def main():
