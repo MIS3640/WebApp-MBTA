4a804a12543df79b442bb7fb4651d3172bf51580
bsung1 <70121996+bsung1@users.noreply.github.com>
2020-10-27 11:09:46 -0400

    update

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        location = request.form["location"]
+        mbta, wheelchair = find_stop_near(location)
+        return render_template("mbta_station.html", mbta=mbta, wheelchair=wheelchair)
+    return render_template("index.html")
