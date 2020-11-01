f3dfa62d6b97611eaa34c7017380fe8d0cc45873
Allyson Kummins <akummins2@babson.edu>
2020-11-01 17:12:42 -0500

    Assignment 3 Code

--- app.py
+++ app.py
@@ -2,12 +2,23 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import *
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    try:
+        if request.method != "POST":
+            return render_template('index.html', error=None)
+        elif request.method == "POST":
+            location = request.form["location"]
+            nearest = find_stop_near(location)
+            if nearest:
+                return render_template('nearest_station.html', location=location, nearest=nearest)
+    except:
+        return render_template('error.html')
+
