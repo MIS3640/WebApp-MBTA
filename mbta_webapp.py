fade2597ecaa7a5f909068dd26daa7d9f0997906
DeedeeH999 <70354070+DeedeeH999@users.noreply.github.com>
2020-10-31 21:08:03 -0400

    Update mbta_webapp.py

--- mbta_webapp.py
+++ mbta_webapp.py
@@ -2,14 +2,20 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
 
-@app.route('/hello_world/')
+@app.route("/")
 def hello_world():
-    return render_template('webapp.html')
+    return render_template("webapp.html")
 
-print(hello_world())
+@app.route("/nearest_mbta/", methods=["GET", "POST"])
+def n_mbta():
+    if request.method == "POST":
+        pn = request.form["place_name"]
+        return(find_stop_near(pn))
+    else:
+        return render_template("error.html")  
