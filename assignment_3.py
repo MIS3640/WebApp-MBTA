5caff48364cebf073f9d2ac2359c73a7b4283ed7
daniel-schreier <69647249+daniel-schreier@users.noreply.github.com>
2020-11-01 17:19:25 -0500

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -1,8 +1,6 @@
 import urllib.request
 import json
 from pprint import pprint
-import urllib.parse
-from flask import Flask
 
 MAPQUEST_API_KEY = 'g0tGrEvT7Yr9QaysgxCNbJqMptavv8jf'
 
@@ -17,19 +15,28 @@ def LatLong(data):
     
 LatLong(response_data)
 
-
+import urllib.request
+import urllib.parse
 params = urllib.parse.urlencode({'Babson College': 1})
 url = "http://www.mapquestapi.com/geocoding/v1/address?key=g0tGrEvT7Yr9QaysgxCNbJqMptavv8jf&location=?%s" % params
 with urllib.request.urlopen(url) as f:
      print(f.read().decode('utf-8'))
 
+import urllib.request
+import json
+from pprint import pprint
+url = f'https://api-v3.mbta.com?api_key=bdf5f739ff0441e481b3a440299c9acc&filter[latitude]=42.349396&filter[longitude]=-71.078369&sort=distance'
+f = urllib.request.urlopen(url)
+response_text2 = f.read().decode('utf-8')
+response_data2 = json.dumps(response_text2)
+pprint(response_data2)
 
+
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/hello')
 @app.route('/<name>')
-loc = input ("PlaceName:" )
-print full(loc)
-
-#aa
+location = input ("PlaceName:" )
+print full(location)
