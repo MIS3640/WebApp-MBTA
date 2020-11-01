e698322a1efb0ae0b21b7ca506a2e0ae019255ca
nchapin1 <70173250+nchapin1@users.noreply.github.com>
2020-10-31 22:17:40 -0400

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 4. The Flask backend will handle the request to `POST /nearest_mbta`. Then your app will render a `mbta_station` page for the user - presenting nearest MBTA stop and whether it is wheelchair accessible. In this step, you need to use the code from Part 1.
 5. If something is wrong, the app will render a simple error page, which will include some indication that the search did not work, in addition to a button (or link) that will redirect the user back to the home page.
 """
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, flash, redirect
 from mbta_helper import userlocation, fetchmap, fetchlatlng, fetchmbta
 
 app = Flask(__name__)
@@ -14,17 +14,43 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
 @app.route('/nearest/', methods=["GET", "POST"])
 def search():
     if request.method == "POST":
         location = request.form["location"]
-        # rad = int(request.form["rad"])
+        rad = request.form["rad"]
         map_url = userlocation(location)
         response_data = fetchmap(map_url)
         latlng = fetchlatlng(response_data) 
-        # fetchmbta(latlng,rad)
-        stop_name, stop_accessible = fetchmbta(latlng)
-    return render_template("mbta_station.html", stop_name = stop_name, stop_accessible = stop_accessible)
 
+        # if len(response_data[0]) == 1:
+        #     flash(u'There are no MBTA stops within {rad} miles of your location.', 'error')
+        #     return redirect(url_for('/'))
+        stop_name, stop_accessible = fetchmbta(latlng, rad)
+    return redirect(url_for('nearest_mbta', stop_name = stop_name, stop_accessible = stop_accessible))
+
+@app.route('/nearest_mbta/')
+def result():
+    return render_template("mbta_station.html")
+
+
+
+# @app.route('/nearest/', methods=["GET", "POST"])
+# def search():
+#     if request.method == "POST":
+#         location = request.form["location"]
+#         rad = request.form["rad"]
+#         map_url = userlocation(location)
+#         response_data = fetchmap(map_url)
+#         latlng = fetchlatlng(response_data) 
+#         stop_name, stop_accessible = fetchmbta(latlng, rad)
+#     return render_template("mbta_station.html", stop_name = stop_name, stop_accessible = stop_accessible)
+
+    
     
     
--- mbta_helper.py
+++ mbta_helper.py
@@ -3,13 +3,9 @@ import urllib.parse
 import json
 from pprint import pprint
 
-# TODO:
-#1. Error if location is not good
-
 def userlocation(location):
     # Write a function that takes an address or place name as input and returns a properly encoded URL to make a MapQuest geocode request.
     MAPQUEST_API_KEY = 'GIvTSav4ifuQ6OSObTVe7XzVoW5jB9Zq'
-    # location = {'location': input("Where would you like to search? ")} # need to create error message
     location = {'location': location}
     location = urllib.parse.urlencode(location)
     map_base_url = "http://www.mapquestapi.com/geocoding/v1/address"
@@ -21,7 +17,6 @@ def fetchmap(map_url):
     f = urllib.request.urlopen(map_url)
     response_text = f.read().decode('utf-8')
     response_data = json.loads(response_text)
-    # pprint(response_data)
     return response_data
 
 
@@ -32,7 +27,7 @@ def fetchlatlng(response_data):
     return latlng
 
 
-def fetchmbta(latlng): #add rad
+def fetchmbta(latlng, rad): 
     # Write a function that takes a latitude and longitude and returns the name of the closest MBTA stop and whether it is wheelchair accessible.
     MBTA_API_KEY = 'aa3640ae4bd645cda7374744202f217f'
     MBTA_BASE_URL = 'https://api-v3.mbta.com/stops'
@@ -40,22 +35,18 @@ def fetchmbta(latlng): #add rad
     latitude = latlng.get('lat')
     longitude = latlng.get('lng')
     
-    # rad = float(input("How many miles would you like to search? "))
-    # rad_convert = rad * 2 * 0.01 # default to 0.01 degrees (approximately half a mile)
+    rad_convert = float(rad) * 0.2 # default to 0.01 degrees (approximately half a mile)
 
-    # mbta_url = f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&page[limit]=1&filter[radius]={rad_convert}&filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance'
-    mbta_url = f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&page[limit]=1&filter[radius]=1&filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance'
+    mbta_url = f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&page[limit]=1&filter[radius]={rad_convert}&filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance'
 
     f = urllib.request.urlopen(mbta_url)
     response_text = f.read().decode('utf-8')
     response_data = json.loads(response_text)
-    # pprint(response_data)
     
     # if len(response_data['data']) == 1:
     #     print(f"There are no MBTA stops within {rad} miles of your location.")
 
     stop_info = response_data["data"][0]["attributes"]
-    # pprint(stop_info)
     stop_name = stop_info.get('name')
     stop_accessible = stop_info.get('wheelchair_boarding')
 
@@ -70,12 +61,12 @@ def fetchmbta(latlng): #add rad
 
 def main():
     location = input("Where would you like to search? ")
-    # rad = float(input("How many miles would you like to search? "))
+    rad = input("How many miles would you like to search? ")
     map_url = userlocation(location)
     response_data = fetchmap(map_url)
     latlng = fetchlatlng(response_data)
     # fetchmbta(latlng, rad)
-    fetchmbta(latlng)
+    fetchmbta(latlng, rad)
 
 if __name__ == '__main__':
     main()
