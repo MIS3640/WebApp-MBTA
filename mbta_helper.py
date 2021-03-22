6c1f296ec798b80fdddf6b041ced7529e0a1bc44
James Lee <jlee22@babson.edu>
2021-03-22 16:01:35 -0400

    update

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,10 +1,14 @@
+import urllib.request
+import json
+from pprint import pprint
+
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = "2bvJCZ8VpUKoqpSZXDD6NfXjsaKsLH1q"
+MBTA_API_KEY = "177043a1f6ac4a44ab3d765eff40d67a"
 
 
 # A little bit of scaffolding if you want to use it
@@ -14,7 +18,10 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
 
 
 def get_lat_long(place_name):
@@ -24,7 +31,14 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+    MAPQUEST_API_KEY = "2bvJCZ8VpUKoqpSZXDD6NfXjsaKsLH1q"
+    d = {'place':place_name}
+    result = urllib.parse.urlencode(d)
+    url = f"http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={result}"
+    json = get_json(url)
+    lat = json["results"][0]["locations"][0]["latLng"]["lat"]
+    lng = json["results"][0]["locations"][0]["latLng"]["lng"]
+    return (lat, lng)
 
 
 def get_nearest_station(latitude, longitude):
@@ -34,7 +48,7 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    
 
 
 def find_stop_near(place_name):
@@ -48,8 +62,10 @@ def main():
     """
     You can test all the functions here
     """
-    pass
-
+    # MAPQUEST_API_KEY = "2bvJCZ8VpUKoqpSZXDD6NfXjsaKsLH1q"
+    # url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson+College'
+    # pprint(get_json(url))
+    print(get_lat_long('Babson College'))
 
 if __name__ == '__main__':
     main()
