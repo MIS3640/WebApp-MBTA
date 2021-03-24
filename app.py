a85dce8c385039502e118cbda08f9fa89205c204
MoiseHarari <mharari3@babson.edu>
2021-03-24 15:14:38 -0400

    update

--- app.py
+++ app.py
@@ -4,24 +4,23 @@ Simple "Hello, World" application using Flask
 
 from flask import Flask
 from flask import request
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
 
-@app.route('/templates/index.html')
-@app.route(POST/nearest)
-def hello_world():
-    return 'Hello World!'
+# @app.route('/templates/index.html')
+# @app.route(POST/nearest)
+# def hello_world():
+#     return 'Hello World!'
 
-@app.route('/templates/index.html', method='POST')
+@app.route('/templates/index.html', methods=['POST'])
 def mbta_stop():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+    # error = None
+    try:
+        if request.method == 'POST':
+            place_name = request.form['address']
+            station_name, wheelchair_access = find_stop_near(place_name)
+            return station_name, wheelchair_access
+    except:
+        print('hello')
