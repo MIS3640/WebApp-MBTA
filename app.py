99735a457b4ba5782154690fb6b9b18e0b23f039
MoiseHarari <mharari3@babson.edu>
2021-03-24 15:15:17 -0400

    update

--- app.py
+++ app.py
@@ -5,22 +5,40 @@ Simple "Hello, World" application using Flask
 from flask import Flask
 from flask import request
 from mbta_helper import find_stop_near
+from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def main_page():
+    return render_template('index.html')
 
-# @app.route('/templates/index.html')
-# @app.route(POST/nearest)
-# def hello_world():
-#     return 'Hello World!'
 
-@app.route('/templates/index.html', methods=['POST'])
-def mbta_stop():
-    # error = None
-    try:
-        if request.method == 'POST':
-            place_name = request.form['address']
-            station_name, wheelchair_access = find_stop_near(place_name)
-            return station_name, wheelchair_access
-    except:
-        print('hello')
+@app.route('/nearest', methods=["POST"])
+def get_nearest_stop():
+    place_name = request.form['address']
+    return render_template('station.html', nearest_stop=nearest_stop)
+
+@app.route('/backToHome', methods=["POST"])
+def back_to_home():
+    return redirect(url_for('main_page'))
+
+
+
+
+
+
+
+
+
+
+# @app.route('/templates/index.html', methods=['POST'])
+# def mbta_stop():
+#     # error = None
+#     try:
+#         if request.method == 'POST':
+#             place_name = request.form['address']
+#             station_name, wheelchair_access = find_stop_near(place_name)
+#             return station_name, wheelchair_access
+#     except:
+#         print('hello')
