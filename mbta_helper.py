4b065e352746be5d194c0c0860cf7caef40d7476
Heir03Z <zyao2@babson.edu>
2021-03-26 06:13:52 +0800

    1

--- mbta_helper.py
+++ mbta_helper.py
@@ -8,40 +8,40 @@ MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
 MAPQUEST_API_KEY = "4piLQbWbk4ruiwYv6BxmKeJUQQi3Lkap"
-MBTA_API_KEY = ""
+MBTA_API_KEY = "b6cfab58792c4ac58b111bcd2e717ff1"
 
 
 # A little bit of scaffolding if you want to use it
 
 
-def get_json(url):
-    """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
-    """
-    f = urllib.request.urlopen(url)
-    response_text = f.read().decode('utf-8')
-    response_data = json.loads(response_text)
-    pprint(response_data)
-    return response_data
+# def get_json(url):
+#     """
+#     Given a properly formatted URL for a JSON web API request, return
+#     a Python JSON object containing the response to that request.
+#     """
+#     f = urllib.request.urlopen(url)
+#     response_text = f.read().decode('utf-8')
+#     response_data = json.loads(response_text)
+#     pprint(response_data)
+#     return response_data
 
 
-def get_lat_long(place_name):
-    """
-    Given a place name or address, return a (latitude, longitude) tuple
-    with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding  API URL formatting requirements.
-    """
-    place_name_valid = place_name.replace(" ", "%20")
-    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place_name_valid}'
-    response_data = get_json(url)
-    displayLatLng = response_data["results"][0]["locations"][0]['displayLatLng']
-    lat = displayLatLng["lat"]
-    ltt = displayLatLng["lng"]
-    with open(f'mppp.json', 'w') as file:
-        file.write(json.dumps(response_data, indent=1))
-    return (lat, ltt)
+# def get_lat_long(place_name):
+#     """
+#     Given a place name or address, return a (latitude, longitude) tuple
+#     with the coordinates of the given place.
+#     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
+#     for Mapquest Geocoding  API URL formatting requirements.
+#     """
+#     place_name_valid = place_name.replace(" ", "%20")
+#     url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place_name_valid}'
+#     response_data = get_json(url)
+#     displayLatLng = response_data["results"][0]["locations"][0]['displayLatLng']
+#     lat = displayLatLng["lat"]
+#     ltt = displayLatLng["lng"]
+#     with open(f'mppp.json', 'w') as file:
+#         file.write(json.dumps(response_data, indent=1))
+#     return (lat, ltt)
 
 
 def get_nearest_station(latitude, longitude):
@@ -51,7 +51,14 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    get_nearest_station = get_nearest.replace(" ", "%20")
+    url2 = f'https://api-v3.mbta.com/stops/data/{MBTA_API_KEY}/attributes/latitude/data/{MBTA_API_KEY}/attributes/'
+    response_data2 = get_json(url2)
+    displayStation = response_data2
+    with open(f'MBTA.json', 'w') as file:
+        file.write(json.dumps(response_data2, indent=1))
+
+
 
 
 def find_stop_near(place_name):
@@ -69,5 +76,5 @@ def main():
 
 
 if __name__ == '__main__':
-    print(get_lat_long("babson college"))
+    # print(get_lat_long("babson college"))
     main()
