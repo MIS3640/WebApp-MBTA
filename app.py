b22ab461481726fca7829b8bd9b8d26d2129f4ad
Heir03Z <zyao2@babson.edu>
2021-03-27 15:39:38 +0800

    1

--- app.py
+++ app.py
@@ -1,16 +1,49 @@
 from flask import Flask, render_template, redirect
+from flask import request
 from forms import GeoForm
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = GeoForm()
-    if form.validate():
-        return render_template('mbta.html')
+    # print(request.form)
+    form = GeoForm(request.form)
+    if form.validate(): 
+        place_name = request.form.get('placename')
+        print('placename:', place_name)
+        (station, wheelchair_accessible) = find_stop_near(place_name)
+        print('station:', station)
+        print('wheelchair_accessible:', wheelchair_accessible)
+
+        return render_template('mbta.html',
+                place_name = place_name,
+                station = station,
+                wheelchair_accessible = wheelchair_accessible)
+
     return render_template('index.html', form=form)
 
+
+@app.route('/test', methods=['GET'])
+def test(): 
+    place_name = 'boston'
+    station =  'ABC'
+    wheelchair_accessible = 'Accessible'
+    return render_template('mbta.html',
+            place_name = place_name,
+            station = station,
+            wheelchair_accessible = wheelchair_accessible)
+
+@app.route('/test_empty', methods=['GET'])
+def test_empty(): 
+    place_name = 'boston'
+    station =  ''
+    wheelchair_accessible = ''
+    return render_template('mbta.html',
+            place_name = place_name,
+            station = station,
+            wheelchair_accessible = wheelchair_accessible)
 
 if __name__ == "__main__":
     app.run(debug=True)
