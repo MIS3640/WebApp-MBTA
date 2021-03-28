65a1b651c560c106791af0c325a4fa4916911fb0
lreyesmedrano1 <77638009+lreyesmedrano1@users.noreply.github.com>
2021-03-28 19:19:14 -0400

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,44 +1,59 @@
-# I am currently working on it, I am trying to figure out how to get the forms 
-# to show. I just wanted to make sure you guys saw at least something of progress on this section.
-# only after line 59, before that everything seems all set from my side - Sakshi 
 
-from flask import Flask, render_template, request 
-
-
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
+
+import os
+SECRET_KEY = "abcd"
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 #index page greeting
 @app.route('/')
-def greeting(name=None):
-    if name:
-        return f"hello, {name}!"
-    return "Hello! Welcome to the site!"
+def greeting():
+    # if name:
+    #     return f"hello, {name}!"
+    # return "Hello! Welcome to the site!"
 
-
-
-#form Code 
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class MyForm(FlaskForm):
-    Name= StringField('name', validators = [DataRequired()])
-
-@app.route('/location', methods = ['POST'])
-def location(): 
-    form = LocationForm(request.form) 
+    form = MyForm() 
     if request.method == 'POST' and form.validate(): 
-        user = User(form.username.data, form.email.data) 
+        user = User(form.username.data) 
         db_session.add(user) 
         flash('The email address is'" +email+")
         return redirect(url_for('login')) 
-    return render_template('location', form=form) 
+    return render_template('signup.html', form=form) 
 
-@app.route('/form')
-def location_form():
-    return render_template('index.html')
+#form Code 
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextField, SubmitField
+from wtforms.validators import DataRequired, length
+
+class MyForm(FlaskForm):
+    """Contact form."""
+    username = StringField(
+        'Username',
+        [DataRequired()]
+    )
+    
+    body = TextField(
+        'Message',
+        [
+            DataRequired()
+        ]
+    )
+    
+    submit = SubmitField('Submit')
+
+# @app.route('/', methods = ['GET', 'POST'])
+# def location(): 
+    # form = MyForm() 
+    # if request.method == 'POST' and form.validate(): 
+    #     user = User(form.username.data) 
+    #     db_session.add(user) 
+    #     flash('The email address is'" +email+")
+    #     return redirect(url_for('login')) 
+    # return render_template('signup.html', form=form) 
+
 
 @app.route('/data/', methods = ['POST', 'GET'])
 def data():
@@ -56,7 +71,7 @@ app.run(host='localhost', port=5000)
 
 
 # data page
-@app.route('/mbta_station')
+# @app.route('/mbta_station')
 
 
 # ''' THe Flask backend withll dandle the request to POST/nearest_mbta. 
@@ -78,3 +93,4 @@ app.run(host='localhost', port=5000)
 #     myobj = {'somekey': 'somevalue'}
 
 #     x = request.ost(url, data = myobj)
+
