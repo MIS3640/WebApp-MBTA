94cc5b711914e226c5977425fecaa0983afb3276
Raphaeldunand <57508048+Raphaeldunand@users.noreply.github.com>
2021-03-28 13:57:10 -0400

    Finished part 1

--- part1.py
+++ part1.py
@@ -3,16 +3,23 @@ import json
 from pprint import pprint
 import urllib.parse
 
+#example locations
+'Boston,MA'
+'Wellesley,MA'
+'Chelsea,MA'
+'Brookline,MA'
+
+
 lat = 0
 long = 0
 data_latlong = 0
 # import mbta_helper
 # print(mbta_helper.find_stop_near("Boston Common"))
-print('Input location')
+print('Input your current location to find the nearest MBTA stop')
 name = input()
 def lat_long(location): 
     global data_latlong
-    print('The location is ',location)
+    print('Your location is',location)
     MAPQUEST_API_KEY = 'XxtdAMBvWmSKhAKu0wu0kMXQT8gbOp6b'
     # url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
     url = (f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={name}')
@@ -21,7 +28,7 @@ def lat_long(location):
     data_latlong = json.loads(response_text)
     # pprint(response_data)
 
-lat_long(name)
+# lat_long(name)
 
 
 def stop(datas):
@@ -30,20 +37,21 @@ def stop(datas):
     # global long
     lat = datas["results"][0]["locations"][0]['displayLatLng']['lat']
     long = datas["results"][0]["locations"][0]['displayLatLng']['lng']
-    print('lat',lat)
-    print('long',long)
-    # lat = 42.358894
-    # long = -71.056742
-    # print("The latitude is:", datas["results"][0]["locations"][0]['displayLatLng']['lat'])
-    # print("The longitude is:", datas["results"][0]["locations"][0]['displayLatLng']['lng'])
+    # print('lat',lat)
+    # print('long',long)
     url1 = (f"https://api-v3.mbta.com/stops?sort=distance&filter%5Blatitude%5D={lat}&filter%5Blongitude%5D={long}")
     f = urllib.request.urlopen(url1)
     response_text1 = f.read().decode('utf-8')
     response_data1 = json.loads(response_text1)
     # pprint(response_data1)
-    print("Name:", response_data1["data"][0]["attributes"]['name'])
-    print("At street:", response_data1["data"][0]["attributes"]['at_street'])
-    print("On street:", response_data1["data"][0]["attributes"]['on_street'])
+
+    # print("The name of the closest stop to you is", response_data1["data"][0]["attributes"]['name'])
+    stop_name = response_data1["data"][0]["attributes"]['name']
+    stop_atstreet = response_data1["data"][0]["attributes"]['at_street']
+    stop_onstreet = response_data1["data"][0]["attributes"]['on_street'] 
+    print(f'The name of the stop is {stop_name}. It is at the street {stop_atstreet} and on the street {stop_onstreet}.')
+    # print("At street:", response_data1["data"][0]["attributes"]['at_street'])
+    # print("On street:", response_data1["data"][0]["attributes"]['on_street'])
     wheel = response_data1["data"][0]["attributes"]['wheelchair_boarding']
     # print("Has wheelchair access:", response_data1["data"][0]["attributes"]['wheelchair_boarding'])
     if wheel == 0: 
@@ -57,24 +65,19 @@ def stop(datas):
     # print('long1', long)   
 
 
-stop(data_latlong)
+# stop(data_latlong)
 # print('lat2', lat)
 # print('long2', long)
+def main():
+    lat_long(name)
+    stop(data_latlong)
+
+
+if __name__ == "__main__":
+    main()
+
+#example locations
 'Boston,MA'
 'Wellesley,MA'
-# Chelsea,MA
-'Washington,DC'
 'Chelsea,MA'
-
-# MBTA_API_KEY = '78c78742ffb14c3cb6b22071c0986b51'
-# # lat = 42.358894
-# # long = -71.056742
-# # curl -X GET "https://api-v3.mbta.com/stops?sort=distance&filter%5Blatitude%5D=42.358894&filter%5Blongitude%5D=--71.056742" 
-# url1 = (f"https://api-v3.mbta.com/stops?sort=distance&filter%5Blatitude%5D={lat}&filter%5Blongitude%5D={long}")
-# f = urllib.request.urlopen(url1)
-# response_text1 = f.read().decode('utf-8')
-# response_data1 = json.loads(response_text1)
-# # pprint(response_data1)
-# print("At street:", response_data1["data"][0]["attributes"]['at_street'])
-# print("On street:", response_data1["data"][0]["attributes"]['on_street'])
-
+'Brookline,MA'
